4b629bf2cbc3f61b61906ca40e701fd8293a6bd8
Steven Difelippo <matrixdriver04@gmail.com>
2017-08-11 16:04:02 -0400

    fullyworking

--- app.js
+++ app.js
@@ -45,47 +45,35 @@ app.get('/add_senator', function(req, res, id) {
 
 app.post('/add_senator', function(req, res) {
   let newSenator = {
-    "id": req.body.id + 1,
-    "party": req.body.party,
-    "state": req.body.state,
-    "person": {
-      "gender": req.body.gender,
-      "firstname": req.body.name.split(" ")[0],
-      "lastname": req.body.name.split(" ")[1],
-      "birthday": req.body.birthdate
-    },
-  }
-  Senator.create(newSenator).then(function() {
-    res.redirect('/');
-  }).catch(function() {
-    res.render('add_senator', {
-      error: true,
-      senator: newSenator
-    })
-  })
+        "id": req.body.id,
+        "party": req.body.party,
+        "state": req.body.state,
+        "person": { "gender": req.body.gender,
+                    "firstname": req.body.name.split(" ")[0],
+                    "lastname": req.body.name.split(" ")[1],
+                    "birthday": req.body.birthdate },
+        }
+      Senator.create(newSenator).then(function() {
+        res.redirect('/');
+      }).catch(function(e){
+        res.render('add_senator',{error:true, senator:newSenator})
+        console.log("error is ", e)
+      })
 });
+app.get('/:id', function (req, res) {
+  const id = parseInt(req.params.id)
+  Senator.findAndSort({id},function(senator){
+    res.render('specific_senator', {senators : senator})
+});
+});
+    app.post('/:id', function (req, res) {
+      const id = parseInt(req.params.id)
+      Senator.deleteSenator({id:id}, function(){
+        res.redirect('/')
+        console.log('successful deletion of:')
 
-app.get('/:id', function(req, res) {
-  const senatorId = req.params.id;
-  Senator.findSenator({
-    id: senatorId
-  }, function(results) {
-    res.render('specific_senator', {
-      senator: results
+      })
     });
-  })
-});
-
-app.post('/:id', function(req, res) {
-  const senatorId = req.params.id;
-  Senator.deleteSenator({ id: senatorId }, function() {
-    res.redirect('/')
-    console.log('successful deletion!');
-  })(function(err){
-    console.log('Delete unsuccessful');
-  });
-});
-
 // make app listen on a particular port (starts server)
 app.listen(3000, function() {
   console.log('Senator express application running...');
--- models/senators.js
+++ models/senators.js
@@ -3,85 +3,48 @@ mongoose.Promise = require('bluebird');
 const Schema = mongoose.Schema;
 mongoose.connect('mongodb://localhost:27017/senatorsdb');
 
-const senatorSchema = new Schema({
-  id: {
-    type: Number,
-    required: true,
-    unique: true
-  },
-  congress_numbers: String,
-  party: {
-    type: String,
-    required: true
-  },
-  state: {
-    type: String,
-    required: true
-  },
-  person: {
-    gender: {
-      type: String,
-      required: true
-    },
-    firstname: {
-      type: String,
-      required: true
-    },
-    lastname: {
-      type: String,
-      required: true
-    },
-    birthday: {
-      type: Date,
-      required: true
-    },
-    bioguideid: String,
-    cspanid: Number,
-    twitterid: String,
-    youtubeid: String,
-    gender_label: String
-  },
-  phone: {
-    type: String,
-    required: true
-  },
-  extra: {
-    address:  String,
-    contact_form: String,
-    fax: String,
-    office: String
-  },
-  startdate: Date,
-  enddate: Date,
-  description: String,
+const senatorSchema = new Schema ({
+  "id": Number,
+  "party": { type: String, required: true },
+  "state": { type: String, required: true },
+  "person": { "gender": { type: String, required: true },
+              "firstname": { type: String, required: true },
+              "lastname": { type: String, required: true },
+              "birthday": { type: Date, required: true },
+            },
+  "phone": String,
+  "extra": {
+    "address": String,
+    "contact_form": String,
+       "fax": String,
+       "office": String,
+  }
 
-})
+});
 
-senatorSchema.statics.findAndSort = function(find, render) {
+senatorSchema.statics.findAndSort = function (findRestrictions, howToRender) {
   this
-    .find(find)
-    .sort({ 'person.lastname': 1})
-    .then(function(results) {
-      render(results);
+    .find(findRestrictions)
+    .then(function(senators) {
+      howToRender(senators);
     });
 }
 
-senatorSchema.statics.findSenator = function(find, render) {
+senatorSchema.statics.deleteSenator = function (findRestrictions, redirect) {
   this
-    .findOne(find)
-    .then(function(results) {
-      render(results);
+    .deleteOne(findRestrictions)
+    .then(function() {
+      redirect();
+    });
+}
+senatorSchema.statics.findOneSenator = function (findRestrictions, howToRender) {
+  this
+    .findOne(findRestrictions)
+    .then(function(senators) {
+      howToRender(senators);
     });
 }
 
-senatorSchema.statics.deleteSenator = function(find, render) {
-  this
-    .deleteOne(find)
-    .then(function(senator) {
-      render(senator);
-    });
-}
-
-const Senator = mongoose.model('Senator', senatorSchema, 'senators');
+const Senator = mongoose.model('Senator', senatorSchema, 'senators')
 
 module.exports = Senator;
